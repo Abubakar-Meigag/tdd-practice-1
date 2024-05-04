1d6a70f9b7ffb72bb38f91e6062b12f4d5c0e645
Barath Vignarajah <moneyinthesky@gmail.com>
2024-05-04 18:57:04 +0100

    Solution for exercise-1

--- exercise-1/stops-to-destination.js
+++ exercise-1/stops-to-destination.js
@@ -2,6 +2,11 @@ const stations = require('./northern-line.json')
 
 // This function should calculate the number of stops to the destination
 function calculateStopsToDestination(start, end) {
+    if(!stations.includes(start) || !stations.includes(end)) {
+        throw new Error()
+    }
+
+    return Math.abs(stations.indexOf(end) - stations.indexOf(start))
 }
 
 module.exports = calculateStopsToDestination
--- exercise-1/stops-to-destination.test.js
+++ exercise-1/stops-to-destination.test.js
@@ -6,5 +6,28 @@ const calculateStopsToDestination = require("./stops-to-destination")
 // Run tests with: npm test -- stops-to-destination.test.js
 
 describe("stops to destination", () => {
+
+    test("should throw error if start or end parameters are missing", () => {
+        expect(() => calculateStopsToDestination()).toThrow()
+    })
 	
+    test("should return 0 if the start is the same as the end", () => {
+        expect(calculateStopsToDestination("Oval", "Oval")).toBe(0)
+    })
+
+    test("should throw an error if start station is not in list", () => {
+        expect(() => calculateStopsToDestination("Paddington", "Oval")).toThrow()
+    })
+
+    test("should throw an error if end station is not in list", () => {
+        expect(() => calculateStopsToDestination("Oval", "Paddington")).toThrow()
+    })
+
+    test("should return number of stops to the destination", () => {
+        expect(calculateStopsToDestination("Morden", "Balham")).toBe(5)
+    })
+
+    test("should return number of stops to the destination when the destination is before the start", () => {
+        expect(calculateStopsToDestination("Balham", "Morden")).toBe(5)
+    })
 })
