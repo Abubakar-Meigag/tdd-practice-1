741575b32c5c36f20baa8b6463cc27fffa9aa5db
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-05-07 01:09:43 +0100

    done exercise-1

--- exercise-1/stops-to-destination.js
+++ exercise-1/stops-to-destination.js
@@ -1,7 +1,14 @@
-const stations = require('./northern-line.json')
+const stations = require("./northern-line.json");
 
 // This function should calculate the number of stops to the destination
 function calculateStopsToDestination(start, end) {
+  
+  if (!stations.includes(start) || !stations.includes(end)) {
+    throw new Error();
+  }
+
+  return Math.abs(stations.indexOf(end) - stations.indexOf(start))
+
 }
 
-module.exports = calculateStopsToDestination
+module.exports = calculateStopsToDestination;
--- exercise-1/stops-to-destination.test.js
+++ exercise-1/stops-to-destination.test.js
@@ -1,4 +1,4 @@
-const calculateStopsToDestination = require("./stops-to-destination")
+const calculateStopsToDestination = require("./stops-to-destination");
 
 // https://jestjs.io/docs/getting-started
 // https://jestjs.io/docs/expect
@@ -6,5 +6,29 @@ const calculateStopsToDestination = require("./stops-to-destination")
 // Run tests with: npm test -- stops-to-destination.test.js
 
 describe("stops to destination", () => {
-	
-})
+  test("should throw error if start or end parameters are missing", () => {
+    expect(() => calculateStopsToDestination()).toThrow();
+  });
+
+  test("should return 0 if the start is the same as the end", () => {
+    expect(calculateStopsToDestination("Balham", "Balham")).toBe(0);
+  });
+
+  test("should throw an error if start is not in list ", () => {
+    expect(() => calculateStopsToDestination("Paddington", "oval")).toThrow();
+  });
+
+  test("should throw an error if end is not in list ", () => {
+    expect(() => calculateStopsToDestination("oval", "Paddington")).toThrow();
+  });
+
+  test("should return number of stops the destination", () => {
+    expect(calculateStopsToDestination("Morden", "Balham")).toBe(5);
+  });
+
+  test("should return number of stops to the destination when the destination is before the start", () => {
+      expect(calculateStopsToDestination("Balham", "Morden")).toBe(5)
+  })
+
+
+});
