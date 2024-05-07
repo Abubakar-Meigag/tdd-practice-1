9f0e454efa557ba65cab4f84e18834a40c514017
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-05-07 16:09:56 +0100

    done with exercise-2

--- exercise-2/calculate-total.js
+++ exercise-2/calculate-total.js
@@ -4,6 +4,22 @@ const prices = require('./prices.json')
 //  - the basket is an array of objects, where each object contains an item and quantity
 //  - for example: [ { "item": "Milk", "quantity": 2 } ]
 function calculateTotal(basket) {
+      if (basket.length === 0) {
+            throw new Error('Basket must have item');
+      }
+
+      let total = 0;
+
+      basket.forEach(item => {
+        const itemPrice = prices.find((price) => price.item === item.item)
+        if (itemPrice) {
+            total += itemPrice.price * item.quantity
+        } else {
+            throw new Error(`Price not price for item ${item.element}`)
+        }
+      });
+
+      return total;
 }
 
 module.exports = calculateTotal
--- exercise-2/calculate-total.test.js
+++ exercise-2/calculate-total.test.js
@@ -6,4 +6,34 @@ const calculateTotal = require("./calculate-total")
 // Run tests with: npm test -- calculate-total.test.js
 
 describe("calculate total", () => {
+  // should the basket not have item and quantity
+  // should calculate total price for a single item in the basket
+  // should throw an error if basket is not provided
+  // should throw an error if price is not found for an item
+
+
+  test('should calculate total price for a single item in the basket', () => {
+      const basket = [ { "item": "Milk", "quantity": 2 } ]
+      expect(calculateTotal(basket)).toBe(1.78)
+  })
+  
+  test('should calculate total price for a single item in the basket', () => {
+      const basket = [
+            { "item": "Milk", "quantity": 2 },
+            { "item": "Bread", "quantity": 1 }
+      ]
+      expect(calculateTotal(basket)).toBe(2.98)
+  })
+
+  test('should throw an error if basket is not provided', () => {
+      expect(() => calculateTotal().toThrow())
+  })
+
+  test('should throw an error if price is not provided', () => {
+      const basket = [{ item: 'Water', quantity: 1 }];
+      expect(() => calculateTotal(basket).toThrow(Error))
+  })
+
+  
 })
+
